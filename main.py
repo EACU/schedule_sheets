d693e32d7a5ae647985b51b6c05c3bd1d0bb7ac6
Belvedersky <qwink186@icloud.com>
2018-06-01 04:47:22 +0500

    yatypoi

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import logging
 import datetime
 import pygsheets
+import sqlite3
 from flask import Blueprint,render_template, abort
 from jinja2 import TemplateNotFound
 from flask_api import FlaskAPI
-
+import json
 theme = Blueprint(
     'flask-api', __name__,
     url_prefix='/flask-api',
@@ -13,18 +14,23 @@ theme = Blueprint(
 
 app = FlaskAPI(__name__)
 app.blueprints['flask-api'] = theme
-dayname    = ['Понедельник','Вторник','Cреда','Четверг','Пятница','Cуббота']
-nowWeek = datetime.datetime.today().isocalendar()[1]
-evenOdd = ''
-if (nowWeek % 2==0):
-    evenOdd = "Чётная" 
-else:
-    evenOdd = "Нечетная"
+
 
 
 @app.route('/')
 def main():
  return render_template ('hello.html')
+
+dayname    = ['Понедельник','Вторник','Cреда','Четверг','Пятница','Cуббота']
+department = { 1:"D", 2:"E", 3:"F", 4:"G", 5:"H" }
+nowWeek = datetime.datetime.today().isocalendar()[1]
+evenOdd = ''
+if (nowWeek % 2==0):
+    evenOdd = "Чётная" 
+else:
+    evenOdd = "Нечётная"
+gc = pygsheets.authorize(service_file='service_creds.json')
+sh = gc.open('ScheduleInformatics')
 
 
 @app.route('/Info', methods=['GET'])
@@ -39,30 +45,28 @@ def info():
 
 
 
-@app.route('/getData/<course>/<int:group>', methods=['GET'])
-def schedule(course,group):
-    f"""
-    Расписание на неделю 
-    """
-
-    department = { 1:4, 2:5, 3:6, 4:7, 5:8 }
-    gc = pygsheets.authorize(service_file='service_creds.json')
-    sh = gc.open('ScheduleInformatics')
+@app.route('/getData/<int:course>/<int:dept>', methods=['GET'])
+def schedule(course,dept):
     table = f"{course}20({evenOdd})"
     wks = sh.worksheet('title',table)
-    def courses(courses):
-        # Один поток заполняет шесть дней
-        week = {}
-        daylesson  = { 4:9, 13:18, 22:27, 31:36, 40:45, 49:54 }
-        day = 0
-        for key in daylesson.keys():
-            lessons = wks.get_values(start=(key,courses), end=(daylesson[key],courses), returnas='matrix')
-            week[dayname[day]] = lessons
-            day = day+1
-        return week
-    
+    start = f"{department[dept]}54"
+    end = f"{department[dept]}4"
+    week = wks.get_values(start, end, returnas='matrix', majdim='ROWS', value_render='FORMATTED_VALUE')
+    return {"Неделя сейчас" :evenOdd,
+            "Cегодня" : dayname[datetime.datetime.today().weekday()],
+            dayname[0]: week[0:6],
+            dayname[1]: week[9:15],
+            dayname[2]: week[18:24],
+            dayname[3]: week[27:33],
+            dayname[4]: week[36:42],
+            dayname[5]: week[45:51],}
 
-    return  courses(department[group])
+conn = sqlite3.connect('Schedule.sqlite')
+cursor = conn.cursor()
+
+
+conn.close()
+
 
 @app.errorhandler(500)
 def server_error(e):
@@ -74,5 +78,5 @@ def server_error(e):
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8088)
+    app.run(host='127.0.0.1', port=8088,debug=True)
 
